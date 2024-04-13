b8176467a4759eda785079feb72abd4adfc3b134
TopoFabbri <matufabbri@gmail.com>
2024-04-13 00:07:52 -0300

    Added player movement

--- Assets/PlayerController.cs
+++ Assets/PlayerController.cs
@@ -4,12 +4,19 @@ using UnityEngine.InputSystem;
 public class PlayerController : MonoBehaviour
 {
     [SerializeField] private float speed = 5f;
+    [SerializeField] private Vector2 maxMove = new(2, 2);
 
     private Vector2 direction;
 
     private void Update()
     {
         transform.Translate(new Vector3(direction.x, 0, direction.y) * (speed * Time.deltaTime));
+        
+        transform.position = new Vector3(
+            Mathf.Clamp(transform.position.x, -maxMove.x, maxMove.x),
+            transform.position.y, 
+            Mathf.Clamp(transform.position.z, -maxMove.y, maxMove.y)
+            );
     }
 
     public void OnMove(InputValue value)
